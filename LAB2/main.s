fe694b7240ad28e3692a084370101045c03219ef
Gabriel Godinho <gabrielgodinho@alunos.utfpr.edu.br>
2022-10-20 22:57:25 -0300

    lab 2 final

--- LAB2/main.s
+++ LAB2/main.s
@@ -32,6 +32,8 @@
         EXPORT Start                ; Permite chamar a função Start a partir de 
 			                        ; outro arquivo. No caso startup.s
 									
+		EXPORT GPIOPortJ_Handler
+									
 		; Se chamar alguma função externa	
         ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
 									; função <func>
@@ -51,7 +53,18 @@
 
 ; -------------------------------------------------------------------------------
 
-frase_inicio_1		DCB   'C', 'o', 'f', 'r', 'e', ' ', 'a', 'b', 'e', 'r', 't', 'o', ',', 0
+NVIC_EN1_R				EQU 	0xE000E104
+NVIC_PRI12_R			EQU 	0xE000E430
+
+GPIO_PORTJ_AHB_IS_R		EQU 	0x40060404
+GPIO_PORTJ_AHB_IBE_R	EQU 	0x40060408
+GPIO_PORTJ_AHB_IEV_R	EQU		0x4006040C
+GPIO_PORTJ_AHB_IM_R 	EQU		0x40060410
+GPIO_PORTJ_AHB_RIS_R	EQU		0x40060414
+GPIO_PORTJ_AHB_ICR_R	EQU 	0x4006041C
+
+; -------------------------------------------------------------------------------
+frase_inicio_1		DCB   'C', 'o', 'f', 'r', 'e', ' ', 'a', 'b', 'e', 'r', 't', 'o', 0
 frase_inicio_2		DCB   'd', 'i', 'g', 'i', 't', 'e', ' ', 'n', 'o', 'v', 'a', ' ', 's', 'e', 'n', 'h', 'a', ' ', 'p', 'a', 'r', 'a', ' ', 'f', 'e', 'c', 'h', 'a', 'r', ' ', 'o', ' ', 'c', 'o', 'f', 'r', 'e', 0
 fechado_msg				DCB   'C', 'o', 'f', 'r', 'e', ' ', 'f', 'e', 'c', 'h', 'a', 'd', 'o', 0
 fechando			DCB   'C', 'o', 'f', 'r', 'e', ' ', 'f', 'e', 'c', 'h', 'a', 'n', 'd', 'o', 0
@@ -64,7 +77,7 @@ senha_mestra_2		DCB   'm', 'e', 's', 't', 'r', 'a', ':', ' ', 0
 ; R11 senha do usuario
 ; R10 senha tentaiva
 ; R9 tentativas
-; R8 1 para fechado 0 para aberto
+; R8 1 para TRAVADO 0 para SENHA MESTRA
 ; R7 numero digitos
 
 ; Função main()
@@ -73,6 +86,7 @@ Start
 	BL SysTick_Init
 	BL GPIO_Init                 ;Chama a subrotina que inicializa os GPIO
 	BL LCD_Init
+	BL Int_Init
 	
 	MOV R12, #1234
 
@@ -128,6 +142,10 @@ quatro_digitos
 	BL Manter_4_digitos
 soma
 	ADD R7, #1
+	
+	MOV R0, #500			; evitar o bounce
+	BL SysTick_Wait1ms
+	
 	B espera_entrada
 fechar_cofre
 	MOV R0, #0x01
@@ -180,6 +198,10 @@ espera_soltar_f
 	MOV R0, R6
 	BL LCD_Caracter
 	ADD R7, #1
+	
+	MOV R0, #500			; evitar o bounce
+	BL SysTick_Wait1ms
+	
 	B espera_entrada_f
 					
 					
@@ -216,12 +238,140 @@ abre
 	B MainLoop
 	
 Trava_Cofre
+	MOV R8, #1
 	MOV R0, #0x01
 	BL LCD_Instrucao
 	LDR R4, =travado
 	BL Imprime_Frase
+	
+	LDR R0, =GPIO_PORTJ_AHB_ICR_R
+	MOV R1, #0x01
+	STR R1, [R0]
+	
+	LDR R0, =GPIO_PORTJ_AHB_IM_R
+	MOV R1, #0x01
+	STR R1, [R0]
+	
+	MOV R11, #0
+	MOV R9, #1
 loop
+	
+	ADD R11, #1
+	MOV R2, #-1
+	CMP R11, #100
+	ITT EQ
+		MULEQ R9, R2
+		MOVEQ R11, #0
+	BL Pisca_LED
+	
+	CMP R8, #0
+	BEQ Destrava
 	B loop
+	
+Destrava
+	LDR R0, =GPIO_PORTJ_AHB_IM_R
+	MOV R1, #0x00
+	STR R1, [R0]
+	
+inicio
+	MOV R0, #0x01
+	MOV R7, #0
+	BL LCD_Instrucao
+	LDR R4, =senha_mestra_1
+	BL Imprime_Frase
+	
+	MOV R0, #0xC0
+	BL LCD_Instrucao
+	
+	LDR R4, =senha_mestra_2
+	BL Imprime_Frase
+	
+espera_entrada_i
+	ADD R11, #1
+	MOV R2, #-1
+	CMP R11, #50
+	ITT EQ
+		MULEQ R9, R2
+		MOVEQ R11, #0
+	BL Pisca_LED
+	
+	BL Teclas_Input				 ;Chama a subrotina que lê o estado das teclas e coloca o resultado em R0
+	
+	CMP R0, #-1
+	BEQ espera_entrada_i
+	MOV R6, R0
+espera_soltar_i
+	ADD R11, #1
+	MOV R2, #-1
+	CMP R11, #50
+	ITT EQ
+		MULEQ R9, R2
+		MOVEQ R11, #0
+		
+	BL Pisca_LED
+
+	BL Teclas_Input
+	CMP R0, #-1
+	BNE espera_soltar_i
+
+	CMP R7, #0
+	ITT EQ
+		MOVEQ R1, #1000
+		MULEQ R10, R6, R1
+	CMP R7, #1
+	ITT EQ
+		MOVEQ R1, #100
+		MLAEQ R10, R6, R1, R10
+	CMP R7, #2
+	ITT EQ
+		MOVEQ R1, #10
+		MLAEQ R10, R6, R1, R10
+	CMP R7, #3
+	ITTT EQ
+		MOVEQ R1, #1
+		MLAEQ R10, R6, R1, R10
+		BEQ testa
+		
+	ADD R6, #48
+	MOV R0, R6
+	BL LCD_Caracter
+	ADD R7, #1
+	
+	MOV R0, #500			; evitar o bounce
+	BL SysTick_Wait1ms
+	
+	B espera_entrada_i
+testa
+	CMP R10, R12
+	BNE inicio
+	
+	B abre
+	
+Pisca_LED
+	PUSH {LR}
+	
+	CMP R9, #-1
+	ITE EQ
+		MOVEQ R4, #2_11111111
+		MOVNE R4, #2_00000000
+	
+	BL Seg7_Output
+	
+	MOV R0, #1
+	BL SysTick_Wait1ms
+	
+	MOV R5, #2_100000
+	BL PortP_Output
+	
+	MOV R0, #1
+	BL SysTick_Wait1ms
+	
+	MOV R5, #0
+	BL PortP_Output
+
+	POP {LR}
+	BX LR
+		
 	
 Manter_4_digitos
 	PUSH {LR}
@@ -271,6 +421,48 @@ Manter_4_digitos
 	
 	POP {LR}
 	BX LR
+Int_Init
+	LDR R0, =GPIO_PORTJ_AHB_IM_R
+	MOV R1, #0x00
+	STR R1, [R0]
+	
+	LDR R0, =GPIO_PORTJ_AHB_IS_R
+	MOV R1, #0x00
+	STR R1, [R0]
+	
+	LDR R0, =GPIO_PORTJ_AHB_IBE_R
+	MOV R1, #0x00
+	STR R1, [R0]
+	
+	LDR R0, =GPIO_PORTJ_AHB_IEV_R
+	MOV R1, #0x00
+	STR R1, [R0]
+	
+	LDR R0, =GPIO_PORTJ_AHB_ICR_R
+	MOV R1, #0x01
+	STR R1, [R0]
+	
+	LDR R0, =NVIC_EN1_R
+	MOV R1, #0x80000
+	STR R1, [R0]
+	
+	LDR R0, =NVIC_PRI12_R
+	MOV R1, #5
+	LSL R1, #29
+	STR R1, [R0]
+
+	BX LR
+
+GPIOPortJ_Handler
+	LDR R0, =GPIO_PORTJ_AHB_ICR_R
+	MOV R1, #0x01
+	STR R1, [R0]
+	
+	MOV R8, #0
+	
+	BX LR
+	
+
 ; -------------------------------------------------------------------------------------------------------------------------
 ; Fim do Arquivo
 ; -------------------------------------------------------------------------------------------------------------------------	
